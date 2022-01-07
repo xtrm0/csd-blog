d7d1ce42886ed779396aa52694d333733e15f448
Jay Bosamiya <jaybosamiya@gmail.com>
2022-01-07 17:36:45 -0500

    Make sure that moving to a new year works even if forgotten

--- local_server.sh
+++ local_server.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
+for year in website/content/20*; do
+    if [ ! -e "$year/_index.md" ]; then
+        cp website/content/2021/_index.md "$year/_index.md";
+    fi
+done
+
 cd website/ || exit 255
 ../binaries/use_zola serve
 cd ../
